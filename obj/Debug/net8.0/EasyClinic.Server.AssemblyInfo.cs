a24889952834a04b23049a3258d6a7d279fd92b8
hecjav1992 <easydata10@gmail.com>
2025-12-05 06:32:31 -0500

    act controlador back

--- obj/Debug/net8.0/EasyClinic.Server.AssemblyInfo.cs
+++ obj/Debug/net8.0/EasyClinic.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasyClinic.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+000117de279996dadaf71c088347d93deee22f77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c1de061c678dca5403d72ae4c4bd0fd77813b11")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasyClinic.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasyClinic.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
